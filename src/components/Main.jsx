dd20360cd605875393d506f6fa638189b5702df8
Minh Nguyen <lambandwoof@gmail.com>
2022-10-18 13:10:33 -0500

    cloned necesarry files

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { Navbar, Posts, Register } from "./";
+
+
+const Main = () => {
+  return (
+    <div id="main">
+      <Navbar />
+      <Posts />
+      <Register />
+    </div>
+  );
+};
+
+export default Main;
